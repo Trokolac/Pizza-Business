9390e5c6bd7e9577e38118ed7c794152026d6cd6
Trokolac <nemanjaaffiliate@gmail.com>
2020-07-23 16:31:47 +0200

    Installing tables to db/creating users table

---
+++ install.php
@@ -0,0 +1,22 @@
+<?php
+
+$db=require './db.inc.php';
+$conf=require './config.inc.php';
+
+// ADDING USERS TABLE TO DB
+
+$stmt_createUsersTable = $db->prepare("
+    CREATE TABLE IF NOT EXISTS `users`(
+        `id` int AUTO_INCREMENT PRIMARY KEY,
+        `email` varchar(30),
+        `name` varchar(30),
+        `password` varchar(32),
+        `created_at` datetime DEFAULT now(),
+        `updated_at` datetime DEFAULT now() ON UPDATE now(),
+        `deleted_at` datetime DEFAULT NULL  
+    )
+");
+
+$stmt_createUsersTable->execute();
+
+var_dump( $db->errorInfo() );
