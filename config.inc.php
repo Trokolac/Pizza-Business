59f248d922e6d9c1d3a241d639d505cde93efddf
Trokolac <nemanjaaffiliate@gmail.com>
2020-07-27 21:38:45 +0200

    DB debuging for heroku

--- config.inc.php
+++ config.inc.php
@@ -4,7 +4,7 @@
 
 return[
     "db_host" => "eu-cdbr-west-03.cleardb.net",
-    "db_name" => "pizza_business",
+    "db_name" => "heroku_1753f8fa38b2769",
     "db_user" => "b62e0e5e6cd8a8",
     "db_pass" => "86318451", 
     "admin_name" => "Admin",
